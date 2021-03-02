610c9deedaa237a74ff4b8247111bfe0c1837e2a
melvincwng <melvincwng@users.noreply.github.com>
2021-03-02 15:43:03 +0800

    Revert to original teardown.js file/code

--- test/teardown.js
+++ test/teardown.js
@@ -1,9 +1,3 @@
-const { MongoMemoryServer } = require("mongodb-memory-server");
-
 module.exports = async () => {
-  const mongoServer = new MongoMemoryServer();
-  const mongoUri = await mongoServer.getUri();
-  process.env.MONGODB_URI = mongoUri;
-  // Set reference to mongo server in order to close the server during teardown
-  global.__MONGOSERVER__ = mongoServer;
+  await global.__MONGOSERVER__.stop();
 };
